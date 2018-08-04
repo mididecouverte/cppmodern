5f418e1a5a1b3fa42742b72d6b06aa8b1bc719f6
Frederic Jacob <frederic.jacob.78@gmail.com>
2018-08-03 22:50:09 -0400

    Add set example

---
+++ containers/set/main.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main (int argc, char *argv[])
+{
+    /* Simple int list */
+    set<int> simpleSet;
+    cout << "Size:" << simpleSet.size() << endl;
+
+    /* Insertion */
+    simpleSet.insert(88);
+    cout << "Size:" << simpleSet.size() << endl;
+
+    /* Cannot add the same value 2 times*/
+    simpleSet.insert(88);
+    cout << "Size:" << simpleSet.size() << endl;
+
+    /* Vector static initializer */
+    set<int> initializedSet = {1, 2, 3, 4};
+    cout << "Size:" << initializedSet.size() << endl;
+
+    /* Delete operation */
+    cout << "Size:" << initializedSet.size() << endl;
+    initializedSet.erase(2);
+    cout << "Size:" << initializedSet.size() << endl;
+
+    /* Test if value in set */
+    cout << "Count:" << initializedSet.count(4) << endl;
+
+    /* For loop */
+    for (auto i: simpleSet)
+        cout << i << endl;
+    return 0;
+}
