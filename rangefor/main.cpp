5437996de4e25f6e3f2901bbe1e8803d1cf0bbbd
Frederic Jacob <fjacob21@hotmail.com>
2018-10-11 21:38:59 -0400

    Change the ranged for loop to use references

--- rangefor/main.cpp
+++ rangefor/main.cpp
@@ -9,17 +9,17 @@ int main (int argc, char *argv[])
     vector<int> initializedVector = {1, 2, 3, 4};
 
     /* For loop */
-    for (auto i: initializedVector)
+    for (auto& i: initializedVector)
         cout << i << endl;
 
     /* Using initializer list */
-    for (int n : {0, 1, 2, 3, 4, 5}) // the initializer may be a braced-init-list
+    for (const int& n : {0, 1, 2, 3, 4, 5}) // the initializer may be a braced-init-list
         std::cout << n << ' ';
     std::cout << std::endl;
 
     /* Using array initialized with initializer list */
     int a[] = {0, 1, 2, 3, 4, 5};
-    for (int n : a)
+    for (int& n : a)
         std::cout << n << ' ';
     std::cout << '\n';
 
